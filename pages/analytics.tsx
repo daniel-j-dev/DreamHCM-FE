6fe18ab1f4a82f97813f760d831b64db4611ba8b
daniel-j-dev <danielaugustusjohnson@gmail.com>
2022-09-24 16:22:31 -0400

    Added base files for new pages

---
+++ pages/analytics.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Analytics: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>DreamHCM - Analytics</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Analytics
---
+++ pages/dashboard.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Dashboard: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>DreamHCM - Dashboard</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Dashboard
