4b4e364e0a107968c0e9ec8397a895d0f6adb5ac
daniel-j-dev <danielaugustusjohnson@gmail.com>
2022-09-28 05:58:56 -0400

    Basic input validation w/ yup for modal

--- components/NewTMModal.tsx
+++ components/NewTMModal.tsx
@@ -1,5 +1,6 @@
 // Modal for creating a new team member
 import Image from "next/image";
+import { BaseSyntheticEvent } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -7,9 +8,37 @@ import closeIcon from "../assets/close_icon.svg";
 
 const NewTMModal = ({ setShowNewTMM }: any) => {
   // Function(s)
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
+  const onSubmit = (v: Object, e: BaseSyntheticEvent | undefined) => {
+    e?.preventDefault();
   };
+
+  // Form / input validation
+  let inputSchema = yup.object().shape({
+    name: yup
+      .string()
+      .required("A name is required.")
+      .min(1, "Name must be at least 1 character long.")
+      .max(100, "Name can't exceed 100 characters."),
+    currentPosition: yup
+      .string()
+      .required("Current position is required.")
+      .min(1, "Current position must be at least 1 character long.")
+      .max(100, "Current position can't exceed 100 characters."),
+    pay: yup
+      .number()
+      .required("Pay is required.")
+      .min(0, "Pay must be at least 0.")
+      .max(1000000000, "Pay can't exceed 1000000000."),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(inputSchema),
+  });
+
   // JSX
   return (
     <div className="modalOuter">
@@ -22,11 +51,43 @@ const NewTMModal = ({ setShowNewTMM }: any) => {
             height={25}
           />
         </button>
-        <form>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Current position" />
-          <input type="number" placeholder="Annual pay" />
-          <button onClick={(e) => handleSubmit(e)}>Create</button>
+        <form
+          onSubmit={handleSubmit((values, event) => onSubmit(values, event))}
+        >
+          <input
+            type="text"
+            placeholder="Name"
+            {...register("name", { required: true })}
+          />
+          {/* Name input error messages */}
+          {errors?.name?.message ? (
+            <span className="error">{`${errors?.name?.message}`}</span>
+          ) : (
+            <></>
+          )}
+          <input
+            type="text"
+            placeholder="Current position"
+            {...register("currentPosition", { required: true })}
+          />
+          {/* Current position input error messages */}
+          {errors?.currentPosition?.message ? (
+            <span className="error">{`${errors?.currentPosition?.message}`}</span>
+          ) : (
+            <></>
+          )}
+          <input
+            type="number"
+            placeholder="Annual pay"
+            {...register("pay", { required: true })}
+          />
+          {/* Pay input error messages */}
+          {errors?.pay?.message ? (
+            <span className="error">{`${errors?.pay?.message}`}</span>
+          ) : (
+            <></>
+          )}
+          <button>Create</button>
         </form>
       </div>
       <style>{`
@@ -52,6 +113,10 @@ const NewTMModal = ({ setShowNewTMM }: any) => {
         background-color: white;
 
         box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.7);
+      }
+
+      .error {
+        color: red;
       }
  `}</style>
     </div>
