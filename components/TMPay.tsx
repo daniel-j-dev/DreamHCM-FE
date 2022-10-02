e9211667d3228d7c3dfbc7182673e41fc675939f
daniel-j-dev <danielaugustusjohnson@gmail.com>
2022-10-02 09:44:52 -0400

    On pay modal - added text showing full pay including pay adjustments

--- components/TMPay.tsx
+++ components/TMPay.tsx
@@ -1,28 +1,26 @@
 // Form and functionality for updating a team member
-import { BaseSyntheticEvent, useState } from "react";
+import { BaseSyntheticEvent, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { createPayment, getPayments } from "../api/payments";
 
 const TMPay = ({ setShowModal, memberData, setPayments }: any) => {
+  // Estimated weekly pay
+  const basePay: number = Number((memberData.pay / 52).toFixed(2));
+
   // State
   const [payMods, setPayMods] = useState([]); // Pay modifications
+  const [adjPay, setAdjPay] = useState(basePay); // "Adjusted pay" (total pay including all adjustments)
 
-  // Function(s)
-  const fullSubmit = async (v: any, e: BaseSyntheticEvent | undefined) => {
+  // Submit payment
+  const paySubmit = async (v: any, e: BaseSyntheticEvent | undefined) => {
     e?.preventDefault();
-
-    // Get amount after pay adjustments
-    let totalPay = v.originalAmount;
-
-    //@ts-ignore
-    for (let m of payMods) totalPay += m.value;
 
     // Format
     const newPayment = {
       teamMemberId: memberData._id,
-      payAmount: totalPay,
+      payAmount: adjPay,
       originalAmount: v.originalAmount,
       modifications: [...payMods],
     };
@@ -38,15 +36,19 @@ const TMPay = ({ setShowModal, memberData, setPayments }: any) => {
     }
   };
 
+  // Submit pay modification / adjustment
   const modSubmit = (v: any, e: BaseSyntheticEvent | undefined) => {
     e?.preventDefault();
 
     // Add to state
     //@ts-ignore
     setPayMods([...payMods, { ...v, modificationType: "number" }]);
+
+    // Reset form values
+    modForm.reset();
   };
 
-  // Form / input validation
+  // Input validation for "Base pay" input
   let baseSchema = yup.object().shape({
     originalAmount: yup
       .number()
@@ -55,6 +57,11 @@ const TMPay = ({ setShowModal, memberData, setPayments }: any) => {
       .max(1000000000, "payAmount can't exceed 1000000000."),
   });
 
+  const baseForm = useForm({
+    resolver: yupResolver(baseSchema),
+  });
+
+  // Input validation for pay adjustment inputs
   let modSchema = yup.object().shape({
     reason: yup
       .string()
@@ -68,13 +75,20 @@ const TMPay = ({ setShowModal, memberData, setPayments }: any) => {
       .max(1000000000, "V can't exceed 1000000000."),
   });
 
-  const baseForm = useForm({
-    resolver: yupResolver(baseSchema),
-  });
-
   const modForm = useForm({
     resolver: yupResolver(modSchema),
   });
+
+  // Update "adjPay" when either "payMods" or the base pay input changes
+  useEffect(() => {
+    // Add all pay modification values
+    let totalPay: number = Number(baseForm.getValues("originalAmount"));
+    //@ts-ignore
+    for (let m of payMods) totalPay += m.value;
+
+    // Update "adjPay" state
+    setAdjPay(Number(totalPay.toFixed(2)));
+  }, [payMods, baseForm.watch("originalAmount")]);
 
   // JSX
   return (
@@ -83,13 +97,13 @@ const TMPay = ({ setShowModal, memberData, setPayments }: any) => {
 
       <form
         onSubmit={baseForm.handleSubmit((values, event) =>
-          fullSubmit(values, event)
+          paySubmit(values, event)
         )}
       >
         <input
           type="number"
           placeholder="Base pay"
-          defaultValue={(memberData.pay / 52).toFixed(2)} // Assumes "memberData.pay" is annual salary & payments are weekly.
+          defaultValue={basePay} // Assumes "memberData.pay" is annual salary & payments are weekly.
           step="0.01"
           {...baseForm.register("originalAmount", { required: true })}
         />
@@ -100,6 +114,7 @@ const TMPay = ({ setShowModal, memberData, setPayments }: any) => {
           <></>
         )}
 
+        <span className="totalPay">Total pay: ${adjPay}</span>
         <button className="actionBtns">Submit</button>
       </form>
 
@@ -124,6 +139,7 @@ const TMPay = ({ setShowModal, memberData, setPayments }: any) => {
         <input
           type="number"
           placeholder="Pay adjustment"
+          step="0.01"
           {...modForm.register("value", { required: true })}
         />
         {/* Pay input error messages */}
@@ -172,9 +188,14 @@ const TMPay = ({ setShowModal, memberData, setPayments }: any) => {
         .error {
             color: red;
         }
-.modForm {
-  margin-top: 15px;
-}
+
+        .totalPay {
+          margin-bottom: 10px;
+        }
+
+        .modForm {
+          margin-top: 15px;
+        }
         .modsWrap {
           margin-top: 15px;
         }
